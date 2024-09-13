bc7d4072f5756bb6fb64433ce2a755a32c9e58bc
Benjamin Stalder <benjamin.stalder@swisscom.com>
2024-09-13 18:13:04 +0200

    change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			'sans': ['"Montserrat"', 'ui-sans-serif', 'system-ui'],
+			'serif': ['"Vollkorn"', 'ui-serif', 'Georgia'],
+		},
+		extend: {
+			keyframes: {
+				zoom: {
+					'0%': { transform: 'scale(1, 1)' },
+					'50%': { transform: 'scale(1.2, 1.2)' },
+					'100%': { transform: 'scale(1, 1)' },
+				}
+			},
+			animation: {
+				zoom: 'zoom 20s ease infinite',
+			}
+		},
 	},
 	plugins: [require("daisyui")],
 }
